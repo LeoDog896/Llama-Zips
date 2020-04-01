1d14d1e095b3eadbfe9334c11b2536e437b5f9f3
Itsgithubfellow <59983463+Itsgithubfellow@users.noreply.github.com>
2020-04-01 19:23:06 -0400

    Script changes

--- Utils/Scripts/Player.gd
+++ Utils/Scripts/Player.gd
@@ -30,7 +30,7 @@ func _physics_process(delta):
 		velocity.x = 0
 		sprite.play("standing")
 	if Input.is_action_pressed("ui_up") and is_on_floor():
-		velocity.y = -580
+		velocity.y = -610
 	elif is_on_floor():
 		velocity.y = 0
 	move_and_slide(velocity, Vector2(0, -1), false, 4, 0.785398, false)
@@ -46,7 +46,7 @@ func _process(_delta):
 	elif velocity.x > 0:
 		sprite.set_flip_h(false)
 	if Input.is_action_pressed("ui_up") and is_on_floor():
-		velocity.y = -680
+		velocity.y = -610
 
 var collision_point
 
