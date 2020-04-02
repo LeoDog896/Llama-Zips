9f904ec83227adebda56ad9523df2bdde8b7dd95
LeoDog896 <LeoDog896@hotmail.com>
2020-04-02 19:41:46 -0400

    Crate click event

---
+++ Utils/Scripts/Crate.gd
@@ -0,0 +1,20 @@
+extends RigidBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton \
+	and event.button_index == BUTTON_LEFT \
+	and event.is_pressed():
+		self.on_click()
+
+func on_click():
+	print("Click")
