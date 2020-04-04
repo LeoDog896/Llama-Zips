5deeb581cd6fd7421353542b97b0ae95eea83bbe
LeoDog896 <LeoDog896@hotmail.com>
2020-04-04 19:10:52 -0400

    Remake script

--- Utils/Scripts/Crate.gd
+++ Utils/Scripts/Crate.gd
@@ -5,14 +5,15 @@ var isGrabbed = false
 onready var player : KinematicBody2D = get_node("./../Player")
 onready var player_sprite : AnimatedSprite = get_node("./../Player/AnimatedSprite")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton \
 	and event.button_index == BUTTON_LEFT \
 	and event.is_pressed():
+		call_deferred("on_click")
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed and isGrabbed and event.button_index == 1:
 		call_deferred("on_click")
 
 func _process(delta):
@@ -23,10 +24,15 @@ func _process(delta):
 			self.position = player.position + Vector2(32, -16)
 
 func on_click():
-	self.position = player.position
-	self.collision_layer = 0
-	self.collision_mask = 0
-	self.mode = MODE_STATIC
-	self.sleeping = true
-	isGrabbed = true
-	print(get_parent())
+	if isGrabbed:
+		self.collision_layer = 1
+		self.collision_mask = 1
+		self.mode = MODE_RIGID
+		self.sleeping = false
+		isGrabbed = false
+	else:
+		self.collision_layer = 0
+		self.collision_mask = 0
+		self.mode = MODE_STATIC
+		self.sleeping = true
+		isGrabbed = true
