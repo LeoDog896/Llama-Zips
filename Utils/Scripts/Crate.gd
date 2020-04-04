f7f79d55078d8d7fbf301a25ee29b9ba3eeff380
LeoDog896 <LeoDog896@hotmail.com>
2020-04-04 18:36:35 -0400

    almost done with picking up

--- Utils/Scripts/Crate.gd
+++ Utils/Scripts/Crate.gd
@@ -1,10 +1,8 @@
 extends RigidBody2D
 
+var isGrabbed = false
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+onready var player : KinematicBody2D = get_node("./../Player")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,7 +12,20 @@ func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton \
 	and event.button_index == BUTTON_LEFT \
 	and event.is_pressed():
-		self.on_click()
+		call_deferred("on_click")
+
+func _process(delta):
+	print(self.position)
+	if isGrabbed:
+		self.position = player.position
 
 func on_click():
-	print("Click")
+	self.get_parent().remove_child(self)
+	player.add_child(self)
+	self.position = player.position
+	self.collision_layer = 0
+	self.collision_mask = 0
+	self.mode = MODE_STATIC
+	self.sleeping = true
+	isGrabbed = true
+	print(get_parent())
