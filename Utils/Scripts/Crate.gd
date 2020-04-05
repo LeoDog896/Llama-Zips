112fc2b65321af40f1607ef60c370f4fd104c1f2
LeoDog896 <LeoDog896@hotmail.com>
2020-04-05 18:42:26 -0400

    Organization

--- Utils/Scripts/Crate.gd
+++ Utils/Scripts/Crate.gd
@@ -1,20 +1,20 @@
 extends RigidBody2D
 
 var isGrabbed = false
+var to_trigger_click = false
 
 onready var player : KinematicBody2D = get_node("./../Player")
 onready var player_sprite : AnimatedSprite = get_node("./../Player/AnimatedSprite")
-
 
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton \
 	and event.button_index == BUTTON_LEFT \
 	and event.is_pressed():
-		call_deferred("on_click")
+		to_trigger_click = true
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and isGrabbed and event.button_index == 1:
-		call_deferred("on_click")
+		to_trigger_click = true
 
 func _process(delta):
 	if isGrabbed:
@@ -23,16 +23,25 @@ func _process(delta):
 		else:
 			self.position = player.position + Vector2(32, -16)
 
-func on_click():
-	if isGrabbed:
-		self.collision_layer = 1
-		self.collision_mask = 1
-		self.mode = MODE_RIGID
-		self.sleeping = false
-		isGrabbed = false
-	else:
-		self.collision_layer = 0
-		self.collision_mask = 0
-		self.mode = MODE_STATIC
-		self.sleeping = true
-		isGrabbed = true
+func _integrate_forces(state):
+	if to_trigger_click:
+		print(isGrabbed)
+		to_trigger_click = false
+		if isGrabbed:
+			var xform = state.get_transform()
+			state.set_transform(xform)
+			if player_sprite.flip_h:
+				xform.origin = player.position + Vector2(-80, -16)
+			else:
+				xform.origin = player.position + Vector2(80, -16)
+			self.collision_layer = 1
+			self.collision_mask = 1
+			self.mode = MODE_RIGID
+			self.sleeping = false
+			isGrabbed = false
+		else:
+			self.collision_layer = 0
+			self.collision_mask = 0
+			self.mode = MODE_STATIC
+			self.sleeping = true
+			isGrabbed = true
