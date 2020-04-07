4beaf58470318c2431f41a60041bbbfb4fc859d9
LeoDog896 <LeoDog896@hotmail.com>
2020-04-07 18:50:06 -0400

    Fixed picking up item bug

--- Utils/Scripts/Crate.gd
+++ Utils/Scripts/Crate.gd
@@ -8,28 +8,21 @@ onready var player_sprite : AnimatedSprite = get_node("./../Player/AnimatedSprit
 
 func _input_event(_viewport, event, _shape_idx):
 	if Input.is_action_pressed("ui_left_mouse"):
-		to_trigger_click = true
+		pick()
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and isGrabbed and event.button_index == 1:
-		to_trigger_click = true
+		pick()
 var pos
 
-func _physics_process(_delta):
-	print(self.position)
-	if isGrabbed:
-		if player_sprite.flip_h:
-			global_transform.origin = player.position + Vector2(-32, -16)
-		else:
-			global_transform.origin = player.position + Vector2(32, -16)
-	if not isGrabbed and to_trigger_click:
+func pick():
+	if not isGrabbed:
 		self.collision_layer = 0
 		self.collision_mask = 0
 		self.mode = RigidBody2D.MODE_STATIC
 		self.sleeping = true
 		isGrabbed = true
-		to_trigger_click = false
-	elif isGrabbed and to_trigger_click:
+	elif isGrabbed:
 		pos = player.position
 		if player_sprite.flip_h:
 			global_transform.origin = player.position + Vector2(-80, 0)
@@ -38,9 +31,15 @@ func _physics_process(_delta):
 		self.mode = RigidBody2D.MODE_RIGID
 		self.sleeping = false
 		isGrabbed = false
-		to_trigger_click = false
 		player.position = pos
-		print((self.position - get_global_mouse_position()).normalized())
 		self.apply_central_impulse((self.position - get_global_mouse_position()).normalized())
 		self.collision_layer = 1
 		self.collision_mask = 1
+
+func _physics_process(_delta):
+
+	if isGrabbed:
+		if player_sprite.flip_h:
+			global_transform.origin = player.position + Vector2(-32, -16)
+		else:
+			global_transform.origin = player.position + Vector2(32, -16)
