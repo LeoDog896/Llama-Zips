611f562d50da436267779ac5cd14ff7a454f4391
LeoDog896 <LeoDog896@hotmail.com>
2020-04-24 18:34:34 -0400

    quick changes

--- Utils/Scripts/Player.gd
+++ Utils/Scripts/Player.gd
@@ -74,7 +74,7 @@ func _physics_process(delta):
 			sprite.play("standing")
 	if Input.is_action_pressed("ui_up") and is_on_floor():
 		velocity.y = -jump
-	if Input.is_action_just_pressed("gravity_switch") and can_jump:
+	if Input.is_action_just_pressed("gravity_switch"):
 		toggle_gravity()
 	elif is_on_floor():
 		velocity.y = 0
